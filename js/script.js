4b5637bcabe0de017e4aabbea67314b4e2b658bd
Wendy <wendyc8035@queenstechhs.org>
2019-02-07 23:06:01 +0000

    Day 2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Calma","Hold You","My Way","Dust in the Wind","Love to Love you Baby","Sweet Child O’ Mine","I Wanna Hold Your Hand","No More Modelz","El Condor Herido","Persona Ideal","Soffry Soffry Catch Monkey"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+var images_urls = ["","","","","","","","","","",""];
+var artists = ["Pedro Capo & Farruko","Gyptian","Frank Sinatra","Kansas","Donna Summer","Gun’s and Roses","The Beatles","J. Cole","Diomedes Diaz","Orquesta Adolescentes","Ikenga Super Stars Of Africa"];
+var song_lengths = ["","","","","","","","","","",""];
+var links = ["","","","","","","","","","",""];
 
 
 
